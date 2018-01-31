3c2765e5ada68950975b853dd73f9d1282961f11
김경준 <kkchlove@gimgyeongjun-ui-MacBook-Pro.local>
2018-01-31 16:23:15 +0900

    *drop and some file modify

--- examples/kubernetes/tables_schema/add_index_statistical_group_data_1.sql
+++ examples/kubernetes/tables_schema/add_index_statistical_group_data_1.sql
@@ -1,17 +1,17 @@
-ALTER TABLE tbl_metering_ac_group_data_5_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_15_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_30_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_1_hour ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_1_day ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_1_month ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_ac_group_data_1_year ADD INDEX (created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_5_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_15_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_30_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_1_hour ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_1_day ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_1_month ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_ac_group_data_1_year ADD INDEX index_1(created_ts, logical_id, group_id);
 
-ALTER TABLE tbl_metering_dc_group_data_5_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_15_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_30_min ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_1_hour ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_1_day ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_1_month ADD INDEX (created_ts, logical_id, group_id);
-ALTER TABLE tbl_metering_dc_group_data_1_year ADD INDEX (created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_5_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_15_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_30_min ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_1_hour ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_1_day ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_1_month ADD INDEX index_1(created_ts, logical_id, group_id);
+ALTER TABLE tbl_metering_dc_group_data_1_year ADD INDEX index_1(created_ts, logical_id, group_id);
 
 
